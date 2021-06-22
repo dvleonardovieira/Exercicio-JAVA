6b9e7d7f1e2cb98b3bf930a19750c25d285dc493
dvleonardovieira <leonardo.adsistemas@gmail.com>
2021-06-21 22:36:23 -0300

    PrimeiroCommit

---
+++ Controlador.java
@@ -0,0 +1,24 @@
+package Encapsulamento;
+
+public interface Controlador {
+	public abstract void ligar();
+
+	public abstract void deligar();
+
+	public abstract void abrirMenu();
+
+	public abstract void fecharMenu();
+
+	public abstract void maisVolume();
+
+	public abstract void menosVolme();
+
+	public abstract void ligarMudo();
+
+	public abstract void deligarMudo();
+
+	public abstract void play();
+
+	public abstract void pause();
+
+}
---
+++ Starter_teste.java
@@ -0,0 +1,14 @@
+package Encapsulamento;
+
+public class Starter_teste {
+
+	public static void main(String[] args) {
+		ControleRemoto clt = new ControleRemoto();
+		clt.deligar();
+		clt.maisVolume();
+		clt.play();
+		clt.abrirMenu();
+		clt.fecharMenu();
+	}
+
+}
